b3e3a1546ef4cb59f8cc5daff41255a81ffca47e
victoriia-h <gl9victoria@gmail.com>
2024-10-12 19:29:38 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';  // Імпортуємо Header
+import Content from './components/Content';  // Імпортуємо Content
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Content />
+        </div>
+    );
+}
+
+export default App;
